04c3cabfa6cafda95f2c2227060eb995d893f7d9
viguime <jvictormguimaraes@gmail.com>
2022-03-24 12:39:43 -0300

    Sass loader

--- 01-github-explorer/webpack.config.js
+++ 01-github-explorer/webpack.config.js
@@ -30,7 +30,7 @@ module.exports = {
                 use: 'babel-loader',
             },
             {
-                test: /\.css$/,
+                test: /\.scss$/,
                 exclude: /node_modules/,
                 use: ['style-loader','css-loader'],
             }
